991dba1feb93c40289783b3d6d12dc92bf10a5b3
ftkvyn <ftkvyn@gmail.com>
2017-05-07 20:24:32 +0200

    initial files

--- app/tasks/pipeline.js
+++ app/tasks/pipeline.js
@@ -31,6 +31,9 @@ var jsFilesToInject = [
   // Dependencies like jQuery, or Angular are brought in here
   'js/dependencies/**/*.js',
 
+  '/js/jquery-3.2.1.min.js',
+  '/js/bootstrap.min.js',
+
   // All of the rest of your client-side js files
   // will be injected here in no particular order.
   'js/**/*.js'
