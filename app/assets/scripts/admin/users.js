a780d0944aa7d589ba9b63a93b4bf8bdda473884
yanyan155 <jm@tut.by>
2017-06-09 03:44:31 +0300

    add script to NewUser form

---
+++ app/assets/scripts/admin/users.js
@@ -0,0 +1,41 @@
+function FilledFields( formData ) {
+	var filled = false;
+	if (formData.name !== '' &&
+		formData.profilePicSmall !== '' &&
+		formData.profilePicLarge !== '' &&
+		formData.login !== '' &&
+		formData.password !== '') {
+		filled = true;
+	}
+	return filled;
+}
+
+$("h3").click(function(){
+	$(".new-user-form").toggle();
+});
+$(".new-user-form").submit(function() {
+	var formData = {
+		name: $("#name").val(),
+		profilePicSmall: $("#profilePicSmall").val(),
+		profilePicLarge: $("#profilePicLarge").val(),
+		login: $("#login").val(),
+		password: $("#password").val(),
+		isAdmin: $("#isAdmin").prop("checked")
+	};
+	if (!FilledFields( formData )) {
+		alert('filled all fields');
+	}
+	else {
+		var posting = $.post("/admin/addUser", formData)
+		posting.fail(function (data){
+			alert(data.message);
+		});
+		posting.done(function(data) {
+			if (!data.success) {
+				alert(data.message);
+			}
+			location.reload();
+		});
+	}
+	return false;
+});
