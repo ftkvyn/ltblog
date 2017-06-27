29347588ed01b088f7e9580ed0fe1171bb05ca12
ftkvyn <ftkvyn@gmail.com>
2017-06-27 22:18:51 +0200

    themes and routes

--- app/api/controllers/ViewsController.js
+++ app/api/controllers/ViewsController.js
@@ -16,7 +16,30 @@ module.exports = {
 		});
 	},
 
-	article: function(req,res){
+	theme: function(req, res, next){
+		if (req.path.match(/\..*/g) || req.path.match(/^\/api\/.*$/)) {
+	        return next();
+	    }
+	    //console.log(req.query["page"]);
+		Theme.findOne({url : req.params.theme})
+		.exec(function(err, data){
+			if(!data){
+      			return next();
+      		}
+			Article.find({where: {theme: data.id} ,limit: 10})
+			.sort('createdAt DESC')
+			.populate('theme')
+			.populate('author')
+			.exec(function(err, articles){
+				return res.view('theme', {articles: articles, theme: data});
+			});
+		});		
+	},	
+
+	article: function(req, res, next){
+		if (req.path.match(/\..*/g) || req.path.match(/^\/api\/.*$/)) {
+	        return next();
+	    }
 		Article.findOne({id: req.params.id})
 		.populate('author')
 		.populate('theme')
@@ -24,6 +47,12 @@ module.exports = {
 			if(err){
       			console.error(err);
       			return res.serverError();
+      		}
+      		if(!data){
+      			return next();
+      		}
+      		if(data.theme.url != req.params.theme){
+      			return next();	
       		}
 			return res.view('article', {article:data, data : {origin: process.env.LTBLOG_ORIGIN || 'http://ltblog-dev.herokuapp.com'}});	
 		});	
